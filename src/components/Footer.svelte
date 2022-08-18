0052b93ad7e04d545e9dba8ba41a6d2c2cc08d36
StephDietz <steph.dietz@vercel.com>
2022-08-18 13:05:33 -0500

    update color of footer border to match header

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,3 +1,3 @@
-<div class="flex w-full flex-col items-center justify-center border-t border-white/30 py-8 px-4">
+<div class="flex w-full flex-col items-center justify-center border-t border-zinc-700 py-8 px-4">
   <div>Deployed on <a class="font-bold" href="https://vercel.com/home">▲ Vercel</a></div>
 </div>
